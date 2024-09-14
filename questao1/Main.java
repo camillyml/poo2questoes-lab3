22219681a3558c7852dad94d56fcd8dda638c97d
Camilly Cruvinel <167707119+camillyml@users.noreply.github.com>
2024-09-13 21:18:11 -0300

    Update Main.java

--- questao1/Main.java
+++ questao1/Main.java
@@ -1,20 +1,20 @@
 
 public class Main {
     public static void main(String[] args) {
-        // Criando um PatoDomestico
+        
         Pato patoDomestico = new PatoDomestico();
         Ave patoAve = new PatoAveWrapper(patoDomestico);
 
         System.out.println("Usando PatoAveWrapper:");
-        patoAve.emitirSom(); // Deve imprimir "Pato doméstico grasnando"
-        patoAve.voar();      // Deve imprimir "Pato doméstico voando"
+        patoAve.emitirSom(); 
+        patoAve.voar();   
 
-        // Criando um PavaoAzul
+      
         Pavao pavaoAzul = new PavaoAzul();
         Ave pavaoAve = new PavaoAveWrapper(pavaoAzul);
 
         System.out.println("\nUsando PavaoAveWrapper:");
-        pavaoAve.emitirSom(); // Deve imprimir "Pavão azul cantando"
-        pavaoAve.voar();      // Não faz nada (método voar não é implementado para Pavao)
+        pavaoAve.emitirSom(); 
+        pavaoAve.voar();      
     }
 }
